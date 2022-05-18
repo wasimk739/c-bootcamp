e0104d3a1721133bd3420048f0b22bc16f90d585
wasimk739 <wasimk739@gmail.com>
2022-05-18 18:02:46 +0530

    fac

---
+++ numPatt123.c
@@ -0,0 +1,27 @@
+// Write a program in C to make such a pattern like right angle triangle with number increased by 1. Go to the editor
+
+// The pattern like :
+
+//    1
+//    2 3
+//    4 5 6
+
+#include <stdio.h>
+int main()
+{
+   int j,i,rows;
+   printf("enter the rows : ");
+   scanf("%d",&rows);
+   for(i=1;i<=rows;i++)
+   {
+     for(j=1;j<i+1;j++)
+     {  
+       printf("%d", );
+       
+      }
+     printf("\n");
+     
+    }
+    return 0;
+} 
+
--- numSpacePattern.c
+++ numSpacePattern.c
@@ -1,3 +1,8 @@
+//    1 
+//   2 3 
+//  4 5 6 
+// 7 8 9 10 
+
 #include <stdio.h>
 int main()
 {
@@ -6,9 +11,11 @@ int main()
    printf("enter the rows : ");
    scanf("%d",&rows);
    for(i=1;i<=rows;i++)
-   {
+   { 
+     printf(" ");
      for(j = 1; j <= i; j++)
      {  
+       printf(" ");
        printf("%d", k++);
       }
      printf("\n");
